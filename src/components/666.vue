1bee44f73f467164e572b5a682c3e9cbc49f62cc
wyh <2504233150@qq.com>
2022-12-06 19:42:55 +0800

    创建仓库

---
+++ src/components/666.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: Yanhuang Wu
+ * @Date: 2022-12-06 19:42:24
+ * @LastEditors: Yanhuang Wu
+ * @LastEditTime: 2022-12-06 19:42:34
+ * @FilePath: \fitness\src\components\666.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by CSG, All Rights Reserved. 
+-->
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
